160f54eaca0330f6c09f3de11655b301edb77b53
Felix Lin <swift.python.geeks@gmail.com>
2019-07-14 22:23:35 -0700

    Custom SQLite Queries and Room DAO

--- app/src/main/java/com/codingwithmitch/foodrecipes/models/Recipe.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/models/Recipe.java
@@ -1,35 +1,49 @@
 package com.codingwithmitch.foodrecipes.models;
 
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
 
 import java.util.Arrays;
 
+@Entity(tableName = "recipes")
 public class Recipe implements Parcelable{
 
+    @PrimaryKey
+    @NonNull
     private String recipe_id;
 
+    @ColumnInfo(name = "title")
     private String title;
 
+    @ColumnInfo(name = "publisher")
     private String publisher;
 
+    @ColumnInfo(name = "image_url")
     private String image_url;
 
+    @ColumnInfo(name = "social_rank")
     private float social_rank;
 
+    @ColumnInfo(name = "ingredients")
     private String[] ingredients;
 
+    @ColumnInfo(name = "timestamp")
+    private int timestamp;
 
-    public Recipe(@NonNull String recipe_id, String title, String publisher, String[] ingredients,
-                  String image_url, float social_rank) {
+    public Recipe(@NonNull String recipe_id, String title, String publisher, String image_url,
+                  float social_rank, String[] ingredients, int timestamp) {
+        this.recipe_id = recipe_id;
         this.title = title;
         this.publisher = publisher;
-        this.ingredients = ingredients;
-        this.recipe_id = recipe_id;
         this.image_url = image_url;
         this.social_rank = social_rank;
+        this.ingredients = ingredients;
+        this.timestamp = timestamp;
     }
 
     public Recipe() {
@@ -42,6 +56,7 @@ public class Recipe implements Parcelable{
         image_url = in.readString();
         social_rank = in.readFloat();
         ingredients = in.createStringArray();
+        timestamp = in.readInt();
     }
 
     @Override
@@ -52,6 +67,7 @@ public class Recipe implements Parcelable{
         dest.writeString(image_url);
         dest.writeFloat(social_rank);
         dest.writeStringArray(ingredients);
+        dest.writeInt(timestamp);
     }
 
     @Override
@@ -70,7 +86,6 @@ public class Recipe implements Parcelable{
             return new Recipe[size];
         }
     };
-
 
     public String getTitle() {
         return title;
@@ -120,6 +135,14 @@ public class Recipe implements Parcelable{
         this.recipe_id = recipe_id;
     }
 
+    public int getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(int timestamp) {
+        this.timestamp = timestamp;
+    }
+
     @Override
     public String toString() {
         return "Recipe{" +
@@ -129,6 +152,7 @@ public class Recipe implements Parcelable{
                 ", image_url='" + image_url + '\'' +
                 ", social_rank=" + social_rank +
                 ", ingredients=" + Arrays.toString(ingredients) +
+                ", timestamp=" + timestamp +
                 '}';
     }
 }
